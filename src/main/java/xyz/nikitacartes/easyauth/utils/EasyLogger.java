a8af65e13affbe73b5ffd7c9ead8625a2280e4c2
NikitaCartes <nikich98@yandex.ru>
2022-06-12 14:06:38 +0300

    Revert changes for 1.18.1

--- src/main/java/xyz/nikitacartes/easyauth/utils/EasyLogger.java
+++ src/main/java/xyz/nikitacartes/easyauth/utils/EasyLogger.java
@@ -1,10 +1,10 @@
 package xyz.nikitacartes.easyauth.utils;
 
-import com.mojang.logging.LogUtils;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EasyLogger {
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void logError(String error) {
         LOGGER.error("[EasyAuth] " + error);
